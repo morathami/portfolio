cca45f642e5c6a30c6bc23277ef0c324a0f7dae5
Thami Endamora Daulay <thamiendamora@gmail.com>
2024-12-01 17:06:57 +0700

    Update GitHub Pages Link

--- src/assets/params.js
+++ src/assets/params.js
@@ -1,61 +1,59 @@
 export const headerTextLst = {
-    "/" : "Hi, I'm Thami!",
-    "/about": "Hi, I'm Thami!",
-    "/contact": "Hi, I'm Thami!",
-    "/lighthearted": "Lighthearted Game",
-    "/overtower": "Overtower Game",
-    "/webglmodel": "WebGL 3D Model",
-    "/cyberbuddy": "Thesis: Cyber Buddy Educational Game",
-    "/ts4fashion":"3D Fashion Modelling",
-    "/edumonster":"Edumonster Game (WIP)",
-    "/pakobox":"Pakobox Game (WIP)"
+    "/portfolio/" : "Hi, I'm Thami!",
+    "/portfolio/about": "Hi, I'm Thami!",
+    "/portfolio/lighthearted": "Lighthearted Game",
+    "/portfolio/overtower": "Overtower Game",
+    "/portfolio/webglmodel": "WebGL 3D Model",
+    "/portfolio/cyberbuddy": "Thesis: Cyber Buddy Educational Game",
+    "/portfolio/ts4fashion":"3D Fashion Modelling",
+    "/portfolio/edumonster":"Edumonster Game (WIP)",
+    "/portfolio/pakobox":"Pakobox Game (WIP)"
 }
 
 export const subheaderTextLst = {
-    "/" : "an aspiring game developer",
-    "/about": "an aspiring game developer",
-    "/contact": "let's keep in touch!",
-    "/lighthearted": "a happiness-themed puzzle platformer game",
-    "/overtower": "a roguelike platformer game",
-    "/webglmodel": "a WebGL-based interactive 3D models",
-    "/cyberbuddy": "",
-    "/ts4fashion":"made with Blender for The Sims 4 in-game clothing",
-    "/edumonster":"a monster-themed teaching simulation game concept",
-    "/pakobox":"a wholesome packaging VR game design concept"
+    "/portfolio/" : "an aspiring game developer",
+    "/portfolio/about": "an aspiring game developer",
+    "/portfolio/lighthearted": "a happiness-themed puzzle platformer game",
+    "/portfolio/overtower": "a roguelike platformer game",
+    "/portfolio/webglmodel": "a WebGL-based interactive 3D models",
+    "/portfolio/cyberbuddy": "",
+    "/portfolio/ts4fashion":"made with Blender for The Sims 4 in-game clothing",
+    "/portfolio/edumonster":"a monster-themed teaching simulation game concept",
+    "/portfolio/pakobox":"a wholesome packaging VR game design concept"
 }
 
 export const titleLst = {
-    "/" : "",
-    "/about": "About | ",
-    "/lighthearted": "Lighthearted Game | ",
-    "/overtower": "Overtower Game | ",
-    "/webglmodel": "WebGL 3D Model | ",
-    "/cyberbuddy": "Thesis: Cyber Buddy Game | ",
-    "/ts4fashion":"The Sims 4 3D Fashion Design | ",
-    "/edumonster":"Edumonster Game | ",
-    "/pakobox":"Pakobox Game | "
+    "/portfolio/" : "",
+    "/portfolio/about": "About | ",
+    "/portfolio/lighthearted": "Lighthearted Game | ",
+    "/portfolio/overtower": "Overtower Game | ",
+    "/portfolio/webglmodel": "WebGL 3D Model | ",
+    "/portfolio/cyberbuddy": "Thesis: Cyber Buddy Game | ",
+    "/portfolio/ts4fashion":"The Sims 4 3D Fashion Design | ",
+    "/portfolio/edumonster":"Edumonster Game | ",
+    "/portfolio/pakobox":"Pakobox Game | "
 }
 
 export const imageLinkLst = {
-    "/" : "",
-    "/about": "",
-    "/lighthearted": "https://live.staticflickr.com/65535/52549227785_3ae4303e43_o.png",
-    "/overtower": "https://live.staticflickr.com/65535/52607041524_2cc0071076_o.png",
-    "/webglmodel": "https://live.staticflickr.com/65535/52606284742_9fd97f2c93_o.png",
-    "/cyberbuddy": "https://live.staticflickr.com/65535/52549020269_b4f5d33047_o.png",
-    "/ts4fashion":"https://live.staticflickr.com/65535/52606280752_0b767a2ab6_o.png",
-    "/edumonster":"https://live.staticflickr.com/65535/52610574859_03fd54229e_o.png",
-    "/pakobox":"https://live.staticflickr.com/65535/52631671757_59f90272e1_o.png"
+    "/portfolio/" : "",
+    "/portfolio/about": "",
+    "/portfolio/lighthearted": "https://live.staticflickr.com/65535/52549227785_3ae4303e43_o.png",
+    "/portfolio/overtower": "https://live.staticflickr.com/65535/52607041524_2cc0071076_o.png",
+    "/portfolio/webglmodel": "https://live.staticflickr.com/65535/52606284742_9fd97f2c93_o.png",
+    "/portfolio/cyberbuddy": "https://live.staticflickr.com/65535/52549020269_b4f5d33047_o.png",
+    "/portfolio/ts4fashion":"https://live.staticflickr.com/65535/52606280752_0b767a2ab6_o.png",
+    "/portfolio/edumonster":"https://live.staticflickr.com/65535/52610574859_03fd54229e_o.png",
+    "/portfolio/pakobox":"https://live.staticflickr.com/65535/52631671757_59f90272e1_o.png"
 }
 
 export const imageCardLst = {
-    "/" : "",
-    "/about": "",
-    "/lighthearted": "",
-    "/overtower": "",
-    "/webglmodel": "",
-    "/cyberbuddy": "https://live.staticflickr.com/65535/52548362607_1a93b67816_o.png",
-    "/ts4fashion":"",
-    "/edumonster":"",
-    "/pakobox":""
+    "/portfolio/" : "",
+    "/portfolio/about": "",
+    "/portfolio/lighthearted": "",
+    "/portfolio/overtower": "",
+    "/portfolio/webglmodel": "",
+    "/portfolio/cyberbuddy": "https://live.staticflickr.com/65535/52548362607_1a93b67816_o.png",
+    "/portfolio/ts4fashion":"",
+    "/portfolio/edumonster":"",
+    "/portfolio/pakobox":""
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,57 +11,57 @@ function Home() {
             <Container style={{marginTop:"120px", maxWidth: "1000px", marginBottom:"120px"}}>
                 <Row className="justify-content-center px-0">
                     <Col>
-                        <Link to="/partyplanner">
+                        <Link to="/portfolio/partyplanner">
                             <Image className="border-radius-20" width={1000} src="https://live.staticflickr.com/65535/54175352936_ac05563390_o.png" />
                         </Link>
                     </Col>
                 </Row>
                 <Row className="justify-content-center px-0 mt-5rem">
                     <Col>
-                        <Link to="/whiskers">
+                        <Link to="/portfolio/whiskers">
                             <Image className="border-radius-20" width={1000} src="https://live.staticflickr.com/65535/54174463347_38ee8515c4_o.png" />
                         </Link>
                     </Col>
                 </Row>
                 <Row className="justify-content-center px-0 mt-5rem">
                     <Col>
-                        <Link to="/cyberbuddy">
+                        <Link to="/portfolio/cyberbuddy">
                             <Image className="border-radius-20" width={1000} src="https://live.staticflickr.com/65535/54175352951_f12d29ddbf_o.png" />
                         </Link>
                     </Col>
                 </Row>
                 <Row className="justify-content-center px-0 mt-5rem">
                     <Col>
-                        <Link to="/edumonster">
+                        <Link to="/portfolio/edumonster">
                             <Image className="border-radius-20" width={462} src="https://live.staticflickr.com/65535/54174481052_9979e86cfa_o.png" />
                         </Link>
                     </Col>
                     <Col style={{textAlign:"right"}} className="px-0">
-                        <Link to="/pakobox">
+                        <Link to="/portfolio/pakobox">
                             <Image className="border-radius-20" width={462} src="https://live.staticflickr.com/65535/54175370236_6bbbfb579f_o.png" />
                         </Link>
                     </Col>
                 </Row>
                 <Row className="justify-content-center px-0 mt-5rem">
                     <Col>
-                        <Link to="/lighthearted">
+                        <Link to="/portfolio/lighthearted">
                             <Image className="border-radius-20" width={300} src="https://live.staticflickr.com/65535/54175626838_c2754c8d93_o.png" />
                         </Link>
                     </Col>
                     <Col style={{textAlign:"center"}}>
-                        <Link to="/overtower">
+                        <Link to="/portfolio/overtower">
                             <Image className="border-radius-20" width={300} src="https://live.staticflickr.com/65535/54174463337_f107bf7a8b_o.png" />
                         </Link>
                     </Col>
                     <Col style={{textAlign:"right"}}>
-                        <Link to="/ts4fashion">
+                        <Link to="/portfolio/ts4fashion">
                             <Image className="border-radius-20" width={300} src="https://live.staticflickr.com/65535/54174504667_2b4e4cbe4b_o.png" />
                         </Link>
                     </Col>
                 </Row>
                 {/*<Row className="justify-content-center px-0 mt-5rem">*/}
                 {/*    <Col>*/}
-                {/*        <Link to="/ts4fashion">*/}
+                {/*        <Link to="/portfolio/ts4fashion">*/}
                 {/*            <Image className="border-radius-20" width={462} src="https://live.staticflickr.com/65535/54175809370_16a0db3862_o.png" />*/}
                 {/*        </Link>*/}
                 {/*    </Col>*/}
